9b81f15ed602fe7e7589bc387f098e8f26de7e0b
gokul1630 <gokulgp163034@gmail.com>
2021-08-06 17:26:20 +0530

    updated routers paths

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const TodoController = require('../controllers/TodoController');
 
-router.get('/', TodoController.get_all_todos);
-router.get('/findOne', TodoController.get_one_todo);
-router.post('/', TodoController.post_new_todo);
-router.patch('/', TodoController.update_todo);
-router.delete('/', TodoController.delete_todo);
+router.post('/getAllTodo', TodoController.get_all_todos);
+router.post('/findOneTodo', TodoController.get_one_todo);
+router.put('/postNewTodo', TodoController.post_new_todo);
+router.patch('/updateTodo', TodoController.update_todo);
+router.delete('/deleteTodo', TodoController.delete_todo);
 
 module.exports = router;
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-router.post('/', UserController.post_new_user);
-router.delete('/', UserController.delete_delete_user);
+router.post('/loginUser', UserController.post_signin_user);
+router.put('/signUpUser', UserController.post_new_user);
+router.delete('/deleteUser', UserController.delete_delete_user);
 
 module.exports = router;
