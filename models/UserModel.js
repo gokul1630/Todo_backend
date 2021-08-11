b2df180a501419ea525f27c5ce7aef69b1d47925
gokul1630 <gokulgp163034@gmail.com>
2021-08-11 14:55:44 +0530

    generate jwt token & password checks for user

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -11,5 +12,14 @@ const UserSchema = Schema({
     },
   ],
 });
+
+UserSchema.methods.getToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.EXPIRES_IN,
+  });
+};
+UserSchema.methods.checkPassword = function (password) {
+  return password === this.password;
+};
 
 module.exports = mongoose.model('User', UserSchema);
