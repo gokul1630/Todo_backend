acb0f3f5624aceaec6d5e8afd748b7fb5a0f3b51
gokul1630 <gokulgp163034@gmail.com>
2021-08-11 14:49:13 +0530

    new route to auth user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,35 +1,56 @@
 const User = require('../models/UserModel');
 const Todo = require('../models/TodoModel');
 
-const loginUser = async (req, res) => {
+const loginUser = async (req, res, next) => {
   const { user, password } = req.body;
-  let users = await User.findOne({ user, password });
-  if (!users) {
-    res.status(404).send("user isn't registred yet");
-  } else {
-    res.json(users);
+
+  if (!user || !password) {
+    res.status(403).send({
+      message: 'Please provide email and password',
+    });
   }
+
+  let users = await User.findOne({ user });
+  if (!users) {
+    res.status(400).send({ message: "Email isn't registred yet" });
+  }
+  const match = users.checkPassword(password);
+
+  if (!match) {
+    res.status(403).send({ message: "Password doesn't match" });
+  }
+  let token = users.getToken();
+  res.status(200).json({ token: token });
 };
 
 const signUpUser = async (req, res) => {
   const { user, password } = req.body;
   let data = await User.create({ user, password });
+  let token = data.getToken();
+  res.json({ token: token });
+};
+
+const me = async (req, res) => {
+  const data = req.user;
   res.json(data);
 };
+
 const deleteUser = async (req, res) => {
   const { userId } = req.body;
   let user = await User.findByIdAndDelete(userId);
   let todo = await Todo.find({ userId });
+
   if (user || todo) {
     todo.forEach((documents) => documents.remove());
     res.json('user deleted');
   } else {
-    res.json('something wentt wrong');
+    res.status(403).send({ message: 'something went wrong' });
   }
 };
 
 module.exports = {
   loginUser,
+  me,
   signUpUser,
   deleteUser,
 };
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const auth = require('../middlewares/auth');
 
 router.post('/loginUser', UserController.loginUser);
+router.post('/me', auth, UserController.me);
 router.put('/signUpUser', UserController.signUpUser);
 router.delete('/deleteUser', UserController.deleteUser);
 
