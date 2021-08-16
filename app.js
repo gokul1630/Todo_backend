fd777a91f76c82113281d73f5ba51c72e886b195
gokul1630 <gokulgp163034@gmail.com>
2021-08-16 18:56:18 +0530

    only db operations in services

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const connect = require("./config/database");
-const TodoRoute = require("./routes/TodoRoute");
-const UserRoute = require("./routes/UserRoute");
-const cors = require("cors");
+const connect = require('./config/database');
+const TodoRoute = require('./routes/TodoRoute');
+const UserRoute = require('./routes/UserRoute');
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 1234;
 
-app.use("/todo", TodoRoute);
-app.use("/user", UserRoute);
+app.use('/todo', TodoRoute);
+app.use('/user', UserRoute);
 
 app.listen(port, async () => {
   await connect();
--- services/UserServices.js
+++ services/UserServices.js
@@ -1,68 +1,43 @@
-const User = require("../models/UserModel");
-const Todo = require("../models/TodoModel");
+const User = require('../models/UserModel');
+const Todo = require('../models/TodoModel');
 
 exports.findUserService = async function (userId) {
   return await User.findById(userId);
 };
 
-exports.SignInService = async function (user) {
+exports.signInService = async function (user) {
   return await User.findOne({ user });
 };
 
-exports.SignUpService = async function (user, password) {
+exports.signUpService = async function (user, password) {
   return await User.create({ user, password });
 };
-exports.DeleteUser = async function (userId) {
-  return await User.findByIdAndDelete(userId);
-};
 
-exports.AddNewTodo = async function (req, res) {
-  let {
+exports.addNewTodo = async function ({
+  todo,
+  description,
+  pending,
+  completed,
+  onGoing,
+  testing,
+  userId,
+}) {
+  const todos = await Todo.create({
     todo,
     description,
-    pending = false,
-    onGoing = false,
-    testing = false,
-    completed = false,
+    pending,
+    completed,
+    onGoing,
+    testing,
     userId,
-  } = req.body;
-  try {
-    const todos = await Todo.create({
-      todo,
-      description,
-      pending,
-      completed,
-      onGoing,
-      testing,
-      userId,
-    });
+  });
 
-    let data = await User.findByIdAndUpdate(userId, {
-      $push: { todoList: todos._id },
-    });
-    if (res) {
-      return await res.json(todos);
-    }
-  } catch (error) {
-    res.status(403).send(error.message);
-  }
+  await User.findByIdAndUpdate(userId, {
+    $push: { todoList: todos._id },
+  });
+  return todo;
 };
 
-exports.getAllTodoService = async function (req, res) {
-  const { userId } = req.body;
-
-  try {
-    await User.findById(userId)
-      .lean()
-      .populate("todoList")
-      .exec((err, post) => {
-        if (post) {
-          res.json(post.todoList);
-        } else {
-          res.status(403).send(err);
-        }
-      });
-  } catch (error) {
-    res.status(403).send(error.message);
-  }
+exports.getAllTodoService = async function (userId) {
+  return await User.findById(userId).lean().populate('todoList').exec();
 };
